0641112daac42dd458214f3d3cb589180931a793
Gregor F <gafrelek@gmail.com>
2024-02-02 00:36:00 +0100

    finished Stopwatch logic

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function Stopwatch() {
   //setting up useStates hook to manage time state.
   // Initialize with 0 to represent the starting time.
   // Running State to manage a boolean state representing running/stopped state
   const [time, setTime] = useState(0);
-  let [running, setRunning] = useState(false);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (running) {
+      intervalId = setInterval(() => {
+        setTime((prevTime) => prevTime + 100); //Increment time by 100 miliseconds
+      }, 100);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [running]);
 
   function stopwatchStart() {
     return setRunning(true);
@@ -19,19 +33,21 @@ function Stopwatch() {
     return setRunning(false), setTime(0);
   }
 
-  while (setRunning == true) {
-    setInterval(() => {
-      setTime();
-    }, 1000); // 1000 milliseconds = 1 second
+  function formatTime(milliseconds) {
+    const minutes = Math.floor(milliseconds / 60000);
+    const seconds = ((milliseconds % 60000) / 1000).toFixed(1);
+    return `${minutes}:${seconds}`;
   }
 
   return (
     <>
-      <h1>stopper</h1>
-      <h2>{time}</h2>
-      <button onClick={stopwatchStart}>START</button>
-      <br />
-      <button onClick={stopwatchStop}>STOP</button>
+      <h1>Stopwatch</h1>
+      <h2>{formatTime(time)}</h2>
+      {!running ? (
+        <button onClick={stopwatchStart}>START</button>
+      ) : (
+        <button onClick={stopwatchStop}>STOP</button>
+      )}
       <br />
       <button onClick={stopwatchReset}>RESET</button>
     </>
