ec95cbeb5449c71dac8a9dbc6c64079dedc7566e
Gregor F <gafrelek@gmail.com>
2024-02-12 05:18:00 +0100

    styling added

--- src/components/Stopwatch.jsx
+++ src/components/Stopwatch.jsx
@@ -40,17 +40,34 @@ function Stopwatch() {
   }
 
   return (
-    <>
-      <h1>Stopwatch</h1>
-      <h2>{formatTime(time)}</h2>
-      {!running ? (
-        <button onClick={stopwatchStart}>START</button>
-      ) : (
-        <button onClick={stopwatchStop}>STOP</button>
-      )}
-      <br />
-      <button onClick={stopwatchReset}>RESET</button>
-    </>
+    // Global container
+    <div className="bg-slate-700 h-screen flex justify-center items-center">
+      <div className="bg-slate-950 border-2 rounded-xl border-slate-800 w-full mx-5 p-6 md:w-2/3">
+        <div className="flex flex-col bg-slate-800 text-white justify-center text-center  rounded-lg shadow-xl h-full space-y-16 py-20">
+          <h2 className="text-7xl">{formatTime(time)}</h2>
+          <div className="flex flex-row justify-around space-x-4 md:pt-20 group">
+            {!running ? (
+              <button
+                className="bg-green-900 w-3/12 py-3 rounded-md border-2 border-green-500 text-xl hover:bg-green-600 duration-300 md:border-4"
+                onClick={stopwatchStart}>
+                START
+              </button>
+            ) : (
+              <button
+                className="bg-red-900 w-3/12 py-3 rounded-md border-2 border-red-500 text-xl hover:bg-red-700 duration-300 md:border-4"
+                onClick={stopwatchStop}>
+                STOP
+              </button>
+            )}
+            <button
+              className="bg-yellow-800 w-3/12 py-3 rounded-md border-2 border-orange-500 text-xl md:border-4 hover:bg-orange-600 duration-300"
+              onClick={stopwatchReset}>
+              RESET
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
